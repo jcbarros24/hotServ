3dbdd877115fd252c8d64179241b2541d918a43d
jcbarros24 <126578116+jcbarros24@users.noreply.github.com>
2023-06-04 20:41:05 -0300

    Commit

--- hotserv/pessoa/Tutor.java
+++ hotserv/pessoa/Tutor.java
@@ -7,21 +7,12 @@ public class Tutor {
     private String email;  
     private String telefone;
 
-<<<<<<< HEAD
     public Tutor(String nome, String cpf, String email, String telefone){ 
-=======
-    public Tutor(String nome,String cpf,String email,String telefone){ 
->>>>>>> 42fd37b36e17e5ddde5c0cea93b42beea12fae04
         this.nome = nome; 
         this.cpf = cpf;
         this.email = email;
         this.telefone = telefone;
     }
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> 42fd37b36e17e5ddde5c0cea93b42beea12fae04
     public String getNome() {
         return nome;
     }
