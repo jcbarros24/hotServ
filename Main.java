4d068e400c7e1c4b43a9f05a522953dd58396e44
jcbarros24 <126578116+jcbarros24@users.noreply.github.com>
2023-06-06 13:03:17 -0300

    Commit

--- Main.java
+++ Main.java
@@ -22,7 +22,7 @@ public class Main {
             hotel.adicionarQuarto(qN);
         }
             
-            do {
+        do {
             System.out.println(" ");
             System.out.println("-------- Hotel Pet Menu --------");
             System.out.println("1. Adicionar reserva");
@@ -35,190 +35,190 @@ public class Main {
 
             scanner.nextLine(); // Consume the newline character
             
-            switch (option) {
-                case 1:
-                    System.out.println(" ");
-                    System.out.println("---- Adicionar Reserva ----");
-                    Quarto room = null;   
-                    int opcaoL = 0;
-                    String roomType = " ";
-                    do {
-                        System.out.println(" ");
-                        System.out.print("Informe o tipo de quarto (Normal/Confort): ");
-                        roomType = scanner.nextLine();
-                        if (roomType.equalsIgnoreCase("Normal")) {
-                            if(hotel.isFullquartosN() == true){ 
-                                if(hotel.getQuartoDisponivelN() == null){ 
-                                    System.out.println("Não possuímos quartos do tipo Normal disponíveis.");
-                                    System.out.println("Para checar a disponibilidade do Hotel, clique na opção 4 do menu.");
-                                    System.err.println("Caso deseja ir para o menu, digite 1. Caso deseja encerrar o programa, digite 2");
-                                    opcaoL = scanner.nextInt();
-                                    break;
-                                }else{
-                                    room = hotel.getQuartoDisponivelN();
-                                }
-                            }else{ 
-                                room = qN;
-                                qN = new Quarto("Normal");
-                                hotel.adicionarQuarto(qN);
-                                i = (i + 1);
-                                qN.setNumQuarto(qN.getNumQuarto() + i);
+        switch (option) {
+            case 1:
+                System.out.println(" ");
+                System.out.println("---- Adicionar Reserva ----");
+                Quarto room = null;   
+                int opcaoL = 0;
+                String roomType = " ";
+            do {
+                System.out.println(" ");
+                System.out.print("Informe o tipo de quarto (Normal/Confort): ");
+                roomType = scanner.nextLine();
+                    if (roomType.equalsIgnoreCase("Normal")) {
+                        if(hotel.isFullquartosN() == true){ 
+                            if(hotel.getQuartoDisponivelN() == null){ 
+                                System.out.println("Não possuímos quartos do tipo Normal disponíveis.");
+                                System.out.println("Para checar a disponibilidade do Hotel, clique na opção 4 do menu.");
+                                System.err.println("Caso deseja ir para o menu, digite 1. Caso deseja encerrar o programa, digite 2");
+                                opcaoL = scanner.nextInt();
+                                break;
+                            }else{
+                                room = hotel.getQuartoDisponivelN();
                             }
+                        }else{ 
+                            room = qN;
+                            qN = new Quarto("Normal");
+                            hotel.adicionarQuarto(qN);
+                            i = (i + 1);
+                            qN.setNumQuarto(qN.getNumQuarto() + i);
                         }
-                        else if (roomType.equalsIgnoreCase("Confort")) {
-                            if(hotel.isFullquartosC() == true){ 
-                                if(hotel.getQuartoDisponivelC() == null){ 
-                                    System.out.println("Não possuímos quartos do tipo Confort disponíveis.");
-                                    System.out.println("Para checar a disponibilidade do Hotel, clique na opção 4 do menu.");
-                                    System.err.println("Caso deseja ir para o menu, digite 1. Caso deseja encerrar o programa, digite 2");
-                                    opcaoL = scanner.nextInt();
-                                    break;
-                                }else{
-                                    room = hotel.getQuartoDisponivelC();
-                                }
-                            }else{ 
-                                room = qC;
-                                qC = new Quarto("Confort");
-                                hotel.adicionarQuarto(qC);
-                                i = (i + 1);
-                                qC.setNumQuarto(qC.getNumQuarto() + i);
-                            }
-                        }
-                        else {
-                            System.out.println("Tipo de quarto inválido.");
-                            System.out.println("Digite um tipo de quarto válido.");
-                        }
-                    }while ((!roomType.equalsIgnoreCase("Normal")) && (!roomType.equalsIgnoreCase("Confort"))|| opcaoL == 1);
-                     
-                    if (opcaoL == 2){
-                        break;
                     }
+                    else if (roomType.equalsIgnoreCase("Confort")) {
+                        if(hotel.isFullquartosC() == true){ 
+                            if(hotel.getQuartoDisponivelC() == null){ 
+                                System.out.println("Não possuímos quartos do tipo Confort disponíveis.");
+                                System.out.println("Para checar a disponibilidade do Hotel, clique na opção 4 do menu.");
+                                System.err.println("Caso deseja ir para o menu, digite 1. Caso deseja encerrar o programa, digite 2");
+                                opcaoL = scanner.nextInt();
+                                break;
+                            }else{
+                                room = hotel.getQuartoDisponivelC();
+                            }
+                        }else{ 
+                            room = qC;
+                            qC = new Quarto("Confort");
+                            hotel.adicionarQuarto(qC);
+                            i = (i + 1);
+                            qC.setNumQuarto(qC.getNumQuarto() + i);
+                        }
+                    }
+                    else {
+                        System.out.println("Tipo de quarto inválido.");
+                        System.out.println("Digite um tipo de quarto válido.");
+                    }
+                }while ((!roomType.equalsIgnoreCase("Normal")) && (!roomType.equalsIgnoreCase("Confort"))|| opcaoL == 1);
+                 
+                if (opcaoL == 2){
+                    break;
+                }
 
-                     System.out.print("Informe o nome do pet: ");
-                     String petName = scanner.nextLine();
+                System.out.print("Informe o nome do pet: ");
+                String petName = scanner.nextLine();
                      
-                     System.out.print("Informe qual o seu pet (Gato / Cachorro): ");
-                     String petType = scanner.nextLine();
-                     while (!petType.equalsIgnoreCase("Cachorro") && !petType.equalsIgnoreCase("Gato")){ 
-                         System.out.println("Informe um tipo de pet válido: ");
-                         petType= scanner.nextLine();
-                         if(petType.equalsIgnoreCase("Cachorro") || petType.equalsIgnoreCase("Gato")){
-                             break;
-                         }
+                System.out.print("Informe qual o seu pet (Gato / Cachorro): ");
+                 String petType = scanner.nextLine();
+                 while (!petType.equalsIgnoreCase("Cachorro") && !petType.equalsIgnoreCase("Gato")){ 
+                     System.out.println("Informe um tipo de pet válido: ");
+                     petType= scanner.nextLine();
+                     if(petType.equalsIgnoreCase("Cachorro") || petType.equalsIgnoreCase("Gato")){
+                         break;
+                    }
                          
-                     }
-                     System.out.print("Informe a idade do pet(Em anos): ");
-                     int petAge = scanner.nextInt();
-                     
-                     System.out.print("Informe o Tamanho do pet em CM: ");
-                     double petHeight = scanner.nextDouble();
+                }
+                System.out.print("Informe a idade do pet(Em anos): ");
+                int petAge = scanner.nextInt();
+                 
+                System.out.print("Informe o Tamanho do pet em CM: ");
+                double petHeight = scanner.nextDouble();
                       
-                     System.out.print("Informe o peso do pet(Em kg): ");
-                     double petWeight = scanner.nextDouble();
-                     scanner.nextLine(); // Consume the newline character
+                System.out.print("Informe o peso do pet(Em kg): ");
+                double petWeight = scanner.nextDouble();
+                scanner.nextLine(); // Consume the newline character
                      
-                     Pet pet;
-                     if (petType.equalsIgnoreCase("Gato")) {
-                         Gato gato = new Gato();
-                         pet = gato;
+                Pet pet;
+                if (petType.equalsIgnoreCase("Gato")) {
+                    Gato gato = new Gato();
+                    pet = gato;
                          
-                     } else {
-                         Cachorro cachorro = new Cachorro();
-                         System.out.print("Informe a raça do cachorro: ");
-                         String dogBreed = scanner.nextLine();
-                         cachorro.setRaca(dogBreed);
-                         pet = cachorro;
-                     } 
-                     pet.setNome(petName);
-                     pet.setIdade(petAge);
-                     pet.setPeso(petWeight);
-                     pet.setTamanho(petHeight);
+                } else {
+                    Cachorro cachorro = new Cachorro();
+                    System.out.print("Informe a raça do cachorro: ");
+                    String dogBreed = scanner.nextLine();
+                    cachorro.setRaca(dogBreed);
+                    pet = cachorro;
+                } 
+                pet.setNome(petName);
+                pet.setIdade(petAge);
+                pet.setPeso(petWeight);
+                pet.setTamanho(petHeight);
                     
-                    System.out.println(" ");
-                    System.out.print("Informe o nome do tutor: ");
-                    String tutorName = scanner.nextLine();
+                System.out.println(" ");
+                System.out.print("Informe o nome do tutor: ");
+                String tutorName = scanner.nextLine();
                     
-                    System.out.print("Informe o e-mail do tutor: ");
-                    String tutorEmail = scanner.nextLine();
+                System.out.print("Informe o e-mail do tutor: ");
+                String tutorEmail = scanner.nextLine();
                     
-                    System.out.print("Informe o CPF do tutor: ");
-                    String tutorCpf = scanner.nextLine();
+                System.out.print("Informe o CPF do tutor: ");
+                String tutorCpf = scanner.nextLine();
 
-                    System.out.print("Informe o telefone do tutor: ");
-                    String tutorPhone = scanner.nextLine();
+                System.out.print("Informe o telefone do tutor: ");
+                String tutorPhone = scanner.nextLine();
                     
-                    Tutor tutor = new TutorBuilder()  // Utilizando o padrão Builder.
-                    .withName(tutorName)
-                    .withCpf(tutorCpf)
-                    .withEmail(tutorEmail)
-                    .withTelefone(tutorPhone)
-                    .build();
+                Tutor tutor = new TutorBuilder()  // Utilizando o padrão Builder.
+                .withName(tutorName)
+                .withCpf(tutorCpf)
+                .withEmail(tutorEmail)
+                .withTelefone(tutorPhone)
+                .build();
                          
                     
-                    System.out.print("Informe o tempo de hospedagem (Em dias): ");
-                    int stayDuration = scanner.nextInt();
+                System.out.print("Informe o tempo de hospedagem (Em dias): ");
+                int stayDuration = scanner.nextInt();
                     
-                    Reserva reserva = new Reserva(pet, room, tutor, stayDuration);
-                    hotel.adicionarReserva(reserva);
-                    System.out.println("Reserva adicionada com sucesso!");
-                    System.out.println("Seu ID de reserva é: "+reserva.getId());
-                    System.out.println("O número do seu quarto é: "+room.getNumQuarto());
-                    break;
-                
-                case 2:
-                    System.out.println("---- Remover Reserva ----");
-                    System.out.print("Informe o número da reserva: ");
-                try{
-                    UUID reservaId2 = UUID.fromString(scanner.nextLine());
-                    System.out.println("O preço da estádia foi de: ");
-                    System.out.println(hotel.calcularPrecoReserva(reservaId2));
-                    hotel.removerReserva(reservaId2);
-                } catch(IllegalArgumentException e){
-                    System.out.println(" ");
-                    System.out.println(" Número de reserva inválido. Caso não possua uma reserva você pode criar na opção 1 do nosso menu.");
-                    System.out.println(" Caso possua, você pode ter escrito de forma errada. Digite 2 no menu e repita o ID da reserva.");
-                    System.out.println(" Lembre-se de colocar no padrão imposto no ID da reserva: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx com os hífens ( - ) nos devidos lugares.");
-                }
-        
-                    break;
-                
-                case 3:
-                    System.out.println("---- Listar Reservas ----");
-                    System.out.println("Iforme o número da sua reserva: ");
-                try{
-                    UUID reservaId = UUID.fromString(scanner.nextLine());
-                    hotel.listarReserva(reservaId);
-                } catch(IllegalArgumentException e){ 
-                    System.out.println(" ");
-                    System.out.println(" Número de reserva inválido. Caso não possua uma reserva você pode criar na opção 1 do nosso menu.");
-                    System.out.println(" Caso possua, você pode ter escrito de forma errada. Digite 3 no menu e repita o ID da reserva.");
-                    System.out.println(" Lembre-se de colocar no padrão imposto no ID da reserva: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx com os hífens ( - ) nos devidos lugares.");
-                }
+                Reserva reserva = new Reserva(pet, room, tutor, stayDuration);
+                hotel.adicionarReserva(reserva);
+                System.out.println("Reserva adicionada com sucesso!");
+                System.out.println("Seu ID de reserva é: "+reserva.getId());
+                System.out.println("O número do seu quarto é: "+room.getNumQuarto());
                 break;
-
-                case 4:
-                    System.out.println("---- Quartos Disponíveis ----");
-                    try {
-                        hotel.listarQuartosDisponiveis();
-                    }catch(Exception e){ 
-                        System.out.println("Ouve um problema ao listar os quartos, fale com o nosso atendedente.");
-                    }
-                    break;
-
-                case 5:
-                    System.out.println("Encerrando o programa...");
-                    break;
-      
-                case 6: 
-                  
-                default:
-                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
-                    break;
+                
+            case 2:
+                System.out.println("---- Remover Reserva ----");
+                System.out.print("Informe o número da reserva: ");
+            try{
+                UUID reservaId2 = UUID.fromString(scanner.nextLine());
+                System.out.println("O preço da estádia foi de: ");
+                System.out.println(hotel.calcularPrecoReserva(reservaId2));
+                hotel.removerReserva(reservaId2);
+            } catch(IllegalArgumentException e){
+                System.out.println(" ");
+                System.out.println(" Número de reserva inválido. Caso não possua uma reserva você pode criar na opção 1 do nosso menu.");
+                System.out.println(" Caso possua, você pode ter escrito de forma errada. Digite 2 no menu e repita o ID da reserva.");
+                System.out.println(" Lembre-se de colocar no padrão imposto no ID da reserva: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx com os hífens ( - ) nos devidos lugares.");
             }
+        
+                break;
+                
+            case 3:
+                System.out.println("---- Listar Reservas ----");
+                System.out.println("Iforme o número da sua reserva: ");
+            try{
+                UUID reservaId = UUID.fromString(scanner.nextLine());
+                hotel.listarReserva(reservaId);
+            } catch(IllegalArgumentException e){ 
+                System.out.println(" ");
+                System.out.println(" Número de reserva inválido. Caso não possua uma reserva você pode criar na opção 1 do nosso menu.");
+                System.out.println(" Caso possua, você pode ter escrito de forma errada. Digite 3 no menu e repita o ID da reserva.");
+                System.out.println(" Lembre-se de colocar no padrão imposto no ID da reserva: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx com os hífens ( - ) nos devidos lugares.");
+            }
+            break;
 
-            System.out.println();
-        } while (option != 5);
+            case 4:
+                System.out.println("---- Quartos Disponíveis ----");
+                try {
+                    hotel.listarQuartosDisponiveis();
+                }catch(Exception e){ 
+                    System.out.println("Ouve um problema ao listar os quartos, fale com o nosso atendedente.");
+                }
+                    break;
 
-        scanner.close();
+            case 5:
+                System.out.println("Encerrando o programa...");
+                break;
+      
+            case 6: 
+                  
+            default:
+                System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+                break;
+        }
+
+        System.out.println();
+    } while (option != 5);
+
+    scanner.close();
     }
 }
